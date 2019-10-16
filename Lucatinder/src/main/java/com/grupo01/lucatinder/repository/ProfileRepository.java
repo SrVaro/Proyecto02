ee42c78a08dea012ba4443136319b637e42991cd
SrVaro <sr.varo07@gmail.com>
2019-10-16 20:39:54 +0200

    ProfileRepository arreglados los imports

--- Lucatinder/src/main/java/com/grupo01/lucatinder/repository/ProfileRepository.java
+++ Lucatinder/src/main/java/com/grupo01/lucatinder/repository/ProfileRepository.java
@@ -1,5 +1,8 @@
 package com.grupo01.lucatinder.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.grupo01.lucatinder.models.Profile;
+
 
 public interface ProfileRepository extends JpaRepository<Profile,Integer>{
 
