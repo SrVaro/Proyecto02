9d5348cf8702b6fda863bc7dc3371ea4497d9ea5
MJotii <mariajosejimenezpereira@gmail.com>
2019-10-16 20:38:54 +0200

    metodo addprofile en interfaz service

--- Lucatinder/src/main/java/com/grupo01/lucatinder/services/ProfileServiceImpl.java
+++ Lucatinder/src/main/java/com/grupo01/lucatinder/services/ProfileServiceImpl.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import com.grupo01.lucatinder.models.Profile;
+
 
 
 @Repository
@@ -13,6 +15,12 @@ public class ProfileServiceImpl implements ProfileService{
 
 	ProfileServiceImpl() {
 
+	}
+
+	@Override
+	public void addProfile(Profile p) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
