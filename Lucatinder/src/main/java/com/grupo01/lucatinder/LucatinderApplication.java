d6981d2583b81481098f97b85347be937d5212c6
SrVaro <sr.varo07@gmail.com>
2019-10-16 19:22:51 +0200

    Conexion con base de datos y propiedades JPA

--- Lucatinder/src/main/java/com/grupo01/lucatinder/LucatinderApplication.java
+++ Lucatinder/src/main/java/com/grupo01/lucatinder/LucatinderApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.grupo01.lucatinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
