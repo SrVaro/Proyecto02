02fe3d986e93ac863f639e63e7d6253cbc270e16
mccurado <mc_curado@hotmail.com>
2019-10-16 20:08:34 +0200

    clase controller mvc y profileserviceimpl

---
+++ Lucatinder/src/main/java/com/grupo01/lucatinder/services/ProfileServiceImpl.java
@@ -0,0 +1,18 @@
+package com.grupo01.lucatinder.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public class ProfileServiceImpl {
+
+	private static final Logger logger = LoggerFactory.getLogger(ProfileServiceImpl.class);
+
+	ProfileServiceImpl() {
+
+	}
+
+}
