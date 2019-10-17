dd1bccf1518aeca9bfcc9981d9dc4f9411a03367
mccurado <mc_curado@hotmail.com>
2019-10-17 12:35:04 +0200

    metodo getProfileSelection

--- Lucatinder/src/main/java/com/grupo01/lucatinder/repository/ProfileRepositoryCustomImpl.java
+++ Lucatinder/src/main/java/com/grupo01/lucatinder/repository/ProfileRepositoryCustomImpl.java
@@ -1,6 +1,33 @@
 package com.grupo01.lucatinder.repository;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.grupo01.lucatinder.models.Profile;
+
 public class ProfileRepositoryCustomImpl implements ProfileRepositoryCustom {
 
+	private Profile profile;
+
+	@PersistenceContext
+	protected EntityManager entityManager;
+
+	public ProfileRepositoryCustomImpl() {
+
+	}
+
+	/**
+	 * @author MC
+	 * @param actualUserId
+	 * @return List<Profile>
+	 */
 	
+	public List<Profile> getProfileSelection(int actualUserId) {
+
+		String hql = "FROM Profile WhERE id_profile <> 'actualUserId'";
+		return (List<Profile>) entityManager.createQuery(hql).getResultList();
+
+	}
 }
