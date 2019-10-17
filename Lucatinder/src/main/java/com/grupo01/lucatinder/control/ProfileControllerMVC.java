419ea8ca7b152c42a1ce8287ea575e22e9efaf0f
mccurado <mc_curado@hotmail.com>
2019-10-17 11:39:09 +0200

    metodo save en controllermvc y html login

--- Lucatinder/src/main/java/com/grupo01/lucatinder/control/ProfileControllerMVC.java
+++ Lucatinder/src/main/java/com/grupo01/lucatinder/control/ProfileControllerMVC.java
@@ -3,21 +3,31 @@ package com.grupo01.lucatinder.control;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-
-
+import com.grupo01.lucatinder.models.Profile;
+import com.grupo01.lucatinder.services.ProfileService;
 
 @Controller("ControllerMVC")
+
 @RequestMapping("/mvc/profile")
 public class ProfileControllerMVC {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ProfileControllerMVC.class);
-	
+
+	private ProfileService profileservice;
+
 	public ProfileControllerMVC() {
-		
+
 	}
 
-	
-	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(@ModelAttribute Profile profile) {
+		logger.info("-- en SAVE");
+		profileservice.addProfile(profile);
+		return "redirect:/index";
+	}
+
 }
