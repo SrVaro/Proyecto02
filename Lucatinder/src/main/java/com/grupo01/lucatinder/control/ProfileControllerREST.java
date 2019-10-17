ad507a7a67b56e2d3eea962485bbb5c04723993b
Andres Rojas <rojas.jorge730@gmail.com>
2019-10-17 19:32:01 +0200

    Update ProfileControllerREST.java

--- Lucatinder/src/main/java/com/grupo01/lucatinder/control/ProfileControllerREST.java
+++ Lucatinder/src/main/java/com/grupo01/lucatinder/control/ProfileControllerREST.java
@@ -2,12 +2,35 @@ package com.grupo01.lucatinder.control;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import com.grupo01.lucatinder.services.ProfileService;
 
 @RestController
 @RequestMapping ("/rest/profile")
 public class ProfileControllerREST {
+	
+	@SuppressWarnings("serial")
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	class ProfileNotFoundException extends RuntimeException {
+
+		public ProfileNotFoundException() {
+			super("User does not exist");
+		}
+	}
 
 	private static final Logger logger = LoggerFactory.getLogger(ProfileControllerREST.class);
+
+	private int actualUserID;
+	
+	@Autowired
+	private ProfileService profileServ;
+
+	public ProfileControllerREST() {
+	}
+
+  
 }
