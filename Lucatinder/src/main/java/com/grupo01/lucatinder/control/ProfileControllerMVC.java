e6bcd4f0b01aef65524bd494a9d182f9b737c896
Andres Rojas <rojas.jorge730@gmail.com>
2019-10-17 11:19:09 +0200

    /new get method on controller

--- Lucatinder/src/main/java/com/grupo01/lucatinder/control/ProfileControllerMVC.java
+++ Lucatinder/src/main/java/com/grupo01/lucatinder/control/ProfileControllerMVC.java
@@ -3,10 +3,11 @@ package com.grupo01.lucatinder.control;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
+import com.grupo01.lucatinder.models.Profile;
 
 @Controller("ControllerMVC")
 @RequestMapping("/mvc/profile")
@@ -17,7 +18,16 @@ public class ProfileControllerMVC {
 	public ProfileControllerMVC() {
 		
 	}
-
-	
+/**
+ * 
+ * @author andres
+ * @return profileForm.html
+ */
+	@GetMapping ("/new")
+	public String addProfile(ModelMap model) {
+		logger.info("-- en NEW");
+		model.addAttribute("profile", new Profile());
+		return "profileForm";
+	}
 	
 }
