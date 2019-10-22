bf36ebc495acaeacb40517ebc47b34d973e94e33
SrVaro <sr.varo07@gmail.com>
2019-10-22 15:23:56 +0200

    login terminado

--- LucatinderClient/src/app/app-routing.module.ts
+++ LucatinderClient/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   // { path: 'kittys', component: KittyListComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'register', component: ProfileFormComponent },
   { path: 'login', component: LoginComponent }
 ];
