4aaa08685eaad3649ffec4ce42162b71e501bc8e
SrVaro <sr.varo07@gmail.com>
2019-10-22 15:57:30 +0200

    profile list

--- LucatinderClient/src/app/profile-list/profile-list.component.ts
+++ LucatinderClient/src/app/profile-list/profile-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Profile } from '../profile';
 
 @Component({
   selector: 'app-profile-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-list.component.css']
 })
 export class ProfileListComponent implements OnInit {
-
+ 
+  @Input() profiles : Profile[];   
+  
   constructor() { }
 
   ngOnInit() {
