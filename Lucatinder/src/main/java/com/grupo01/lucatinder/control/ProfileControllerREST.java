2f449c18ff299a9053c86417b79d0f519bc6cd67
mccurado <mc_curado@hotmail.com>
2019-10-23 09:26:21 +0200

    delete rest

--- Lucatinder/src/main/java/com/grupo01/lucatinder/control/ProfileControllerREST.java
+++ Lucatinder/src/main/java/com/grupo01/lucatinder/control/ProfileControllerREST.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.grupo01.lucatinder.exception.ProfileNotFoundException;
 import com.grupo01.lucatinder.models.Profile;
 import com.grupo01.lucatinder.services.ProfileService;
+
 
 @RestController
 @RequestMapping("/rest/profile")
@@ -86,5 +88,8 @@ public class ProfileControllerREST {
 		return profileServ.getContactList(actualUserID);
 
 	}
-
+	@DeleteMapping("/delete")
+	void delete() {
+		profileServ.deleteProfile(actualUserID);
+	}	
 }
