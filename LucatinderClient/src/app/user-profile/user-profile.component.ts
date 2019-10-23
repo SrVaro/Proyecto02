c01ca42c508e4c9edfbd78b0812a2c14ae1306ef
MJotii <mariajosejimenezpereira@gmail.com>
2019-10-23 12:08:15 +0200

    edit show profile

--- LucatinderClient/src/app/user-profile/user-profile.component.ts
+++ LucatinderClient/src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from 'selenium-webdriver/firefox';
+import { Router } from '@angular/router';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit{
 
-  constructor() { }
+  profile: Profile;
 
-  ngOnInit() {
+  constructor(private router: Router, private profileService: ProfileService) { }
+
+  ngOnInit(): void {
+    this.profileService.getMyProfile().subscribe(result => this.profile = result);
+  }
+
+  onSubmit(){
+    this.profileService.edit().subscribe(result => this.gotoForm);
+  }
+
+  gotoForm(){
+    this.router.navigate(['/register'])
   }
 
 }
